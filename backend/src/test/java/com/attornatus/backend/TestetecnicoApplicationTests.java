d91cec65010821cb20484a24bcc679180a1318cd
Poliane Maria <polianedomonte@gmail.com>
2023-01-19 16:14:30 -0300

    projeto criado

---
+++ backend/src/test/java/com/attornatus/backend/TestetecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestetecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
